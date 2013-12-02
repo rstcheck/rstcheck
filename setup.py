1550660ec3cafbb336f8c15b040dfadba751179a
Steven Myint <git@stevenmyint.com>
2013-12-01 19:37:56 -0800

    Add installer

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import ast
+from distutils import core
+
+
+def version():
+    """Return version string."""
+    with open('rstcheck') as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
+
+
+with open('README.rst') as readme:
+    core.setup(
+        name='rstcheck',
+        version=version(),
+        url='http://github.com/myint/rstcheck',
+        description='Check code blocks in ReStructuredText.',
+        long_description=readme.read(),
+        scripts=['rstcheck'],
+        classifiers=[
+            'License :: OSI Approved :: BSD License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3'
+        ]
+    )
