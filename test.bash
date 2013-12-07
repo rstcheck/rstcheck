f99800f0906ac0b6a20b6f48fb2dfc0ffd241c81
Steven Myint <git@stevenmyint.com>
2013-12-07 13:14:46 -0800

    Print okay when done

--- test.bash
+++ test.bash
@@ -3,3 +3,5 @@
 ./rstcheck.py --strict good.rst
 ./rstcheck.py --strict bad_cpp.rst | grep Error > /dev/null
 ./rstcheck.py --strict bad_python.rst | grep Error > /dev/null
+
+echo -e '\x1b[01;32mOkay\x1b[0m'
