35338ab98fb7301a244a211ea2f93e597b3e756a
Steven Myint <git@stevenmyint.com>
2013-12-08 07:19:55 -0800

    Clean up tags

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ with open('README.rst') as readme:
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 3.2',
             'Programming Language :: Python :: 3.3'
         ],
