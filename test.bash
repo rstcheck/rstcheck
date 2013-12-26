9bd46f6a57d299cf38720dd56a786a7d66a25f1d
Steven Myint <git@stevenmyint.com>
2013-12-26 13:16:34 -0800

    Generalize

--- test.bash
+++ test.bash
@@ -4,8 +4,8 @@
 ./rstcheck.py --strict-rst --strict-warnings unknown.rst 2>&1 \
     | grep 'Unknown' > /dev/null
 ./rstcheck.py --strict-rst --strict-warnings bad_cpp.rst 2>&1 \
-    | grep Error > /dev/null
+    | grep -i error > /dev/null
 ./rstcheck.py --strict-rst --strict-warnings bad_python.rst 2>&1 \
-    | grep Error > /dev/null
+    | grep -i error > /dev/null
 
 echo -e '\x1b[01;32mOkay\x1b[0m'
