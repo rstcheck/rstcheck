6f1f3978f320163c235570a9d88ce4d1eb1be42f
Steven Myint <git@stevenmyint.com>
2013-12-27 11:11:49 -0800

    Make sure exit status is correct

--- test.bash
+++ test.bash
@@ -1,8 +1,16 @@
-#!/bin/bash -ex
+#!/bin/bash -eux
 
 ./rstcheck.py good.rst
 ./rstcheck.py unknown.rst
-! ./rstcheck.py bad_cpp.rst
-! ./rstcheck.py bad_python.rst
+
+if ./rstcheck.py bad_cpp.rst
+then
+    exit 1
+fi
+
+if ./rstcheck.py bad_python.rst
+then
+    exit 1
+fi
 
 echo -e '\x1b[01;32mOkay\x1b[0m'
