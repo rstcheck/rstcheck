85af4826d18d7aea1de98edd4cf0acc19a80b329
Steven Myint <git@stevenmyint.com>
2014-05-30 05:37:43 -0700

    List Python 3.4

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ with open('README.rst') as readme:
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3.2',
             'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
             'Topic :: Software Development :: Quality Assurance',
         ],
         py_modules=['rstcheck'],
