5bc86709ade5acfff5146bef7066fad069f38e08
Steven Myint <git@stevenmyint.com>
2015-02-23 06:02:18 -0800

    Remove Python 3.2 support due to Sphinx
    Sphinx depends on Pygments, which no longer supports Python 3.2.
    
    https://bitbucket.org/birkenfeld/pygments-main/issue/1054/syntaxerror-on-python-32-using-unicode-u

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open('README.rst') as readme:
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Topic :: Software Development :: Quality Assurance',
