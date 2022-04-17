d776315845d31de1815c467e5762173ee44b9c70
Christian Riedel <cielquan@protonmail.com>
2022-04-17 11:34:17 +0200

    update year in license comment in source

--- rstcheck/__init__.py
+++ rstcheck/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # pylint: disable=too-many-lines
 
-# Copyright (C) 2013-2017 Steven Myint
+# Copyright (C) 2013-2022 Steven Myint
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
