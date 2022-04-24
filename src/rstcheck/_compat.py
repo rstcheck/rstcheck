3f3075d752a49ed27cf50beaf5ff08958e3ae92c
Christian Riedel <cielquan@protonmail.com>
2022-04-24 15:10:27 +0200

    use ImportError in _compat to catch errors correctly

--- src/rstcheck/_compat.py
+++ src/rstcheck/_compat.py
@@ -2,17 +2,17 @@
 
 try:
     from importlib.metadata import metadata
-except ModuleNotFoundError:  # pragma: py-gte-38
+except ImportError:  # pragma: py-gte-38
     from importlib_metadata import metadata  # type: ignore[misc]
 
 try:
     from typing import Literal
-except ModuleNotFoundError:  # pragma: py-gte-38
+except ImportError:  # pragma: py-gte-38
     from typing_extensions import Literal  # type: ignore[misc]
 
 try:
     from typing import TypedDict
-except ModuleNotFoundError:  # pragma: py-gte-38
+except ImportError:  # pragma: py-gte-38
     from typing_extensions import TypedDict
 
 
