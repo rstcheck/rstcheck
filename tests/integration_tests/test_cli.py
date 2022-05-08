12be19b60925fd67382b3f2d38b8e44f0252f7a6
Christian Riedel <cielquan@protonmail.com>
2022-05-08 14:10:09 +0200

    add CLI integrations tests for custom directive/role

--- tests/integration_tests/test_cli.py
+++ tests/integration_tests/test_cli.py
@@ -5,7 +5,7 @@ import pytest
 import typer
 import typer.testing
 
-from tests.conftest import TESTING_DIR
+from tests.conftest import EXAMPLES_DIR, TESTING_DIR
 from tests.integration_tests.conftest import ERROR_CODE_REGEX
 
 
@@ -35,3 +35,56 @@ def test_all_bad_examples(
     assert result.exit_code != 0
     assert ERROR_CODE_REGEX.search(result.stdout) is not None
     assert "Success! No issues detected." not in result.stdout
+
+
+def test_custom_directive_and_role(
+    cli_app: typer.Typer, cli_runner: typer.testing.CliRunner
+) -> None:
+    """Test file with custom directive and role."""
+    test_file = EXAMPLES_DIR / "custom" / "custom_directive_and_role.rst"
+
+    result = cli_runner.invoke(cli_app, str(test_file))
+
+    assert result.exit_code != 0
+    assert len(ERROR_CODE_REGEX.findall(result.stdout)) == 4
+
+
+def test_custom_directive_and_role_with_ignore(
+    cli_app: typer.Typer, cli_runner: typer.testing.CliRunner
+) -> None:
+    """Test file with custom directive and role and CLI ignores."""
+    test_file = EXAMPLES_DIR / "custom" / "custom_directive_and_role.rst"
+
+    result = cli_runner.invoke(
+        cli_app,
+        [
+            "--ignore-directives",
+            "my-directive",
+            "--ignore-roles",
+            "some-custom-thing",
+            str(test_file),
+        ],
+    )
+
+    assert result.exit_code == 0
+    assert "Success! No issues detected." in result.stdout
+
+
+def test_custom_directive_and_role_with_config_file(
+    cli_app: typer.Typer, cli_runner: typer.testing.CliRunner
+) -> None:
+    """Test file with custom directive and role and config file."""
+    test_file = EXAMPLES_DIR / "custom" / "custom_directive_and_role.rst"
+    config_file = EXAMPLES_DIR / "custom" / "rstcheck.custom.ini"
+
+    result = cli_runner.invoke(
+        cli_app,
+        [
+            "--config",
+            str(config_file),
+            str(test_file),
+        ],
+    )
+
+    assert "Success! No issues detected." in result.stdout
+    assert result.exit_code == 0
