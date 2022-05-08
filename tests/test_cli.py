6a8bc6f1f71550c47acd808dc50bfe8dabcee44b
Christian Riedel <cielquan@protonmail.com>
2022-05-08 10:20:09 +0200

    add info to test_cli; rename runner var

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,9 @@
-"""Tests for ``cli`` module."""
+"""Tests for ``cli`` module.
+
+``cli.cli`` is not really good testable with unit tests.
+Therfore here only the help message is checked with integration testing style.
+Furthers tests are in the ``integration_tests`` directory.
+"""
 import pytest
 import typer
 import typer.testing
@@ -9,12 +14,12 @@ from rstcheck import _extras, cli
 app = typer.Typer()
 app.command()(cli.cli)
 
-runner = typer.testing.CliRunner()
+cli_runner = typer.testing.CliRunner()
 
 
 def test_cli_help_message() -> None:
     """Test help message."""
-    result = runner.invoke(app, ["--help"])
+    result = cli_runner.invoke(app, ["--help"])
 
     assert result.exit_code == 0
     assert "Enabled features:" in result.stdout
@@ -23,7 +28,7 @@ def test_cli_help_message() -> None:
 @pytest.mark.skipif(not _extras.SPHINX_INSTALLED, reason="Test when sphinx is installed.")
 def test_cli_help_message_with_sphinx() -> None:
     """Test help message when sphinx is installed."""
-    result = runner.invoke(app, ["--help"])
+    result = cli_runner.invoke(app, ["--help"])
 
     assert result.exit_code == 0
     assert "Enabled features:" in result.stdout
@@ -33,7 +38,7 @@ def test_cli_help_message_with_sphinx() -> None:
 @pytest.mark.skipif(not _extras.TOMLI_INSTALLED, reason="Test when tomli is installed.")
 def test_cli_help_message_with_tomli() -> None:
     """Test help message when toml is installed."""
-    result = runner.invoke(app, ["--help"])
+    result = cli_runner.invoke(app, ["--help"])
 
     assert result.exit_code == 0
     assert "pyproject.toml" in result.stdout
