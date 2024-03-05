ba70b3a413e93209c898f06da4b816cdc4ba3b48
Tempookian <124298725+tempookian@users.noreply.github.com>
2024-03-05 09:44:46 +0100

    Add docstring

--- src/rstcheck/__main__.py
+++ src/rstcheck/__main__.py
@@ -1,3 +1,10 @@
+"""Executes CLI application's `main` function.
+
+This module acts as the entry point for the application. When executed as the
+main script, it calls the `main` function from the ._cli submodule to start the
+application.
+"""
+
 from __future__ import annotations
 
 from ._cli import main
